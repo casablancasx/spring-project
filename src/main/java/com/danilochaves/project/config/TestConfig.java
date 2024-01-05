c3a69c02fb410f8caec27a29ecd585aa37a6af0d
Danilo Chaves <danchavesds@gmail.com>
2024-01-05 16:25:57 -0300

    OrderStatus Enum

--- src/main/java/com/danilochaves/project/config/TestConfig.java
+++ src/main/java/com/danilochaves/project/config/TestConfig.java
@@ -3,6 +3,7 @@ package com.danilochaves.project.config;
 
 import com.danilochaves.project.entities.Order;
 import com.danilochaves.project.entities.User;
+import com.danilochaves.project.entities.enums.OrderStatus;
 import com.danilochaves.project.repositories.OrderRepository;
 import com.danilochaves.project.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,9 +28,9 @@ public class TestConfig implements CommandLineRunner {
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
 
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.DELIVERED, u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.PAID,u2);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.SHIPPED ,u1);
 
         userRepository.saveAll(Arrays.asList(u1,u2));
         orderRepository.saveAll(Arrays.asList(o1,o2,o3));
